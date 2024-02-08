c70ed96bad84b5b8a354f80e2179e41d50757efb
Nevena Verbic <nevena.verbic@gmail.com>
2024-02-08 13:41:13 +0100

    Added random.uniform() to generate random float number

--- src/29 Day/generate_random_num.py
+++ src/29 Day/generate_random_num.py
@@ -4,18 +4,27 @@
 
     Output:
 
-    Randomly generated number in range 0, 100:
-    15
+    Randomly generated number (random.randint()) in range 0, 100:
+    31
+    Randomly generated number (random.uniform()) in range 0, 100:
+    42.73
 '''
 
 import random
 
 def generate_random_num(range_arg):
-    ''' Generate random number using random library '''
+    ''' Generate random number using random.randint() '''
     random_num = random.randint(range_arg[0], range_arg[1])
+    return random_num
+
+def generate_random_num_unifrom(range_arg):
+    ''' Generate random number using random.uniform() '''
+    random_num = round(random.uniform(range_arg[0], range_arg[1]), 2)
     return random_num
 
 if __name__ == "__main__":
     range_of_nums = [0, 100]
-    print(f"Randomly generated number in range {range_of_nums[0]}, {range_of_nums[1]}:\n" \
+    print(f"Randomly generated number (random.randint()) in range {range_of_nums[0]}, {range_of_nums[1]}:\n" \
           f"{generate_random_num(range_of_nums)}")
+    print(f"Randomly generated number (random.uniform()) in range {range_of_nums[0]}, {range_of_nums[1]}:\n" \
+          f"{generate_random_num_unifrom(range_of_nums)}")
