cbd0868e0b83b3c39f07cd6b0a9618ca2b123632
Nevena Verbic <nevena.verbic@gmail.com>
2024-02-19 10:01:07 +0100

    Changes in Day 39 Update regular expression to include also apostr. "’", not only "'". Convert text to lower font.

--- src/39 Day/most_common_words.py
+++ src/39 Day/most_common_words.py
@@ -5,67 +5,70 @@
     Output:
     Total number of words in the online book Alice's Adventures in Wonderland, by Lewis Carroll is: 
 
-    The first 50 most common words in the book are:
-    the : 1700
-    and : 866
-    to : 800
-    a : 676
-    of : 611
-    I : 546
+    The first 50 most common words in the book Alice's Adventures in Wonderland, by Lewis Carol:
+    the : 1834
+    and : 935
+    to : 807
+    a : 694
+    of : 634
     it : 543
-    she : 513
-    said : 458
-    in : 417
-    you : 413
-    Alice : 399
-    was : 358
-    that : 299
-    as : 256
-    her : 245
-    with : 223
-    t : 217
-    at : 216
-    s : 210
-    on : 200
-    all : 193
+    she : 541
+    said : 462
+    you : 440
+    in : 432
+    i : 411
+    alice : 386
+    was : 359
+    that : 295
+    as : 273
+    her : 248
+    with : 228
+    at : 225
+    on : 204
+    all : 199
+    this : 179
+    for : 179
     had : 178
-    for : 162
-    not : 162
-    be : 162
-    this : 156
-    or : 147
-    very : 140
-    but : 136
-    they : 133
-    is : 127
-    little : 127
-    so : 126
-    The : 125
+    but : 174
+    not : 173
+    be : 164
+    or : 154
+    so : 153
+    very : 145
+    what : 137
+    they : 132
+    is : 130
+    little : 129
+    he : 123
+    if : 118
     out : 114
-    he : 107
-    about : 101
-    one : 100
-    what : 99
-    up : 99
-    down : 99
-    his : 95
+    one : 105
+    down : 103
+    about : 102
+    up : 101
+    no : 100
+    gutenberg : 97
+    do : 97
+    his : 96
+    then : 94
+    project : 88
     them : 88
     know : 88
+    have : 87
     were : 85
-    Project : 84
-    Gutenberg : 84
-    like : 84
-    have : 83
 '''
 from collections import Counter
 import re
 
 def most_common_words(text):
     ''' Find the most common words in the text '''
-    words = re.findall(r"([a-z0-9'-]+)", text, re.IGNORECASE)
-    print(f"Total number of all words in the online book Alice's Adventures in Wonderland, by Lewis Carol is: {len(words)}\n")
+    words = re.findall(r"([a-z0-9-'’]+)", text, re.IGNORECASE)
 
-    count_words = Counter(words)
+    # Convert each word to lowercase
+    words_lower_case = [word.lower() for word in words]
+    print(f"Total number of all words in the online book Alice's Adventures in Wonderland, by Lewis Carol is: {len(words_lower_case)}\n")
+
+    count_words = Counter(words_lower_case)
 
     print("The first 50 most common words in the book Alice's Adventures in Wonderland, by Lewis Carol:")
     for word, count in count_words.most_common(50):
@@ -76,4 +79,3 @@ if __name__ == "__main__":
     with open(INPUT_FILE_PATH, 'r', encoding='utf-8') as input_file:
         e_book = input_file.read()
     most_common_words(e_book)
-
